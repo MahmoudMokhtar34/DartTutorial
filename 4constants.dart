a8dd137c388a3536cd2b6ffa8184e86e708298cf
MahMokhtar <30509641+MahmoudMokhtar34@users.noreply.github.com>
2020-06-11 10:58:51 +0200

    Update 4constants.dart

--- 4constants.dart
+++ 4constants.dart
@@ -2,18 +2,18 @@
 void main() {
 
 	// final
-	final cityName = 'Mumbai';
+	final cityName = 'Mumbai';  //intialized only when accessed (not used => not created)
 	//	name = 'Peter';     // Throws an error
 
 	final String countryName = 'India';
 
 	// const
-	const PI = 3.14;
+	const PI = 3.14;  //intialized at compile time (created always at compile time)
 	const double gravity = 9.8;
 }
 
 class Circle {
 
 	final color = 'Red';
-	static const PI = 3.14;
+	static const PI = 3.14; //Const in class must be static also
 }
